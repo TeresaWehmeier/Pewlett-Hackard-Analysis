01c2440c85f5c595fe6abdada23c8e7a2245deb1
Teresa Wehmeier <88757040+TeresaWehmeier@users.noreply.github.com>
2021-12-09 21:35:11 -0600

    added images folder

--- Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -1,199 +1,90 @@
--- Create tables for PH-EmployeeDB
-CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no),
-	UNIQUE (dept_name)
-);
-
--- Create employee table in PH-EmployeeDB
-CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR(40) NOT NULL,
-	last_name VARCHAR(40) NOT NULL,
-	gender VARCHAR(1) NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (EMP_no)
-);
-
--- Create Dept_Manager table
-CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	TO_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
--- Create Salary table
-CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	TO_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
-
--- Create Dept_Emp table
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
-	from_date DATE NOT NULL,
-	TO_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
--- Create Titles table
-CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	TO_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title, from_date)
-);
-
--- select statement to check 
-SELECT first_name, last_name 
-FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'
-;
-
-== Retirement Eligibility
--- select statement to check 
-SELECT count(first_name)
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT *
-FROM retirement_info
-;
-
-DROP TABLE retirement_info;
-
--- Create retirement_info table 
-SELECT emp_no, first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
--- Select retirment info
-SELECT * FROM retirement_info;
-
--- Joining departments and dept_manager tables
-SELECT departments.dept_name,
-	dept_manager.emp_no,
-	dept_manager.from_date,
-	dept_manager.to_date
-FROM departments
-INNER JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no;
-
---Joining retirement_info and dept_emp tables
-SELECT ri.emp_no,
-	ri.first_name,
-	ri.last_name,
-	de.to_date
-FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no;
-
-SELECT d.dept_name,
-	dm.emp_no,
-	dm.from_date,
-	dm.to_date
-FROM departments as d
-INNER JOIN dept_manager as dm
-ON d.dept_no = dm.dept_no;
-
-SELECT ri.emp_no,
-	ri.first_name,
-	ri.last_name,
-de.to_date
-INTO current_emp
-FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no
-WHERE de.to_date = ('9999-01-01');
-
--- Employee count by department number
-SELECT de.dept_no, COUNT(ce.emp_no)
-INTO count_by_department
-FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no
-ORDER BY de.dept_no;
-
-SELECT * FROM salaries
-ORDER BY to_date DESC;
-
-SELECT e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.gender,
-	s.salary,
-	de.to_date
-INTO emp_info
+--SQL query DELIVERABLE 1 steps 1 - 7
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	t.title, 
+	t.from_date, 
+	t.to_date
+INTO retirement_titles
 FROM employees as e
-INNER JOIN salaries as s
-ON (e.emp_no = s.emp_no)
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
-AND (de.to_date = '9999-01-01');
+INNER JOIN	titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
 
--- List of managers per department
-SELECT dm.dept_no,
-		d.dept_name,
-		dm.emp_no,
-		ce.last_name,
-		ce.first_name,
-		dm.from_date,
-		dm.to_date
-INTO manager_info
-FROM dept_manager as dm
-	INNER JOIN departments as d
-		ON (dm.dept_no = d.dept_no)
-	INNER JOIN current_emp as ce
-		ON (dm.emp_no = ce.emp_no);
-		
-SELECT ce.emp_no,
-	ce.first_name,
-	ce.last_name,
-	d.dept_name
-INTO dept_info
-FROM current_emp as ce
+-- Check table file
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct ON with Orderby to remove duplicate rows DELIVERABLE 1, 8-14
+SELECT DISTINCT ON (rt.emp_no)
+	rt.emp_no, 
+	rt.first_name, 
+	rt.last_name, 
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Check unique_tables creation
+SELECT * FROM unique_titles;
+
+-- select the number of retiring employees by title DELIVERABLE 1, 15-21
+SELECT COUNT(emp_no) "Eligible Retirees", title "Title"
+INTO retiring_titles
+FROM unique_titles
+GROUP BY "Title"
+ORDER BY "Eligible Retirees" DESC;
+
+-- Check retiring_titles creation
+SELECT * FROM retiring_titles;
+
+
+-- Create mentorship eligibility table DELIVERABLE 2, 1-11
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
 INNER JOIN dept_emp as de
-ON (ce.emp_no = de.emp_no)
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND t.to_date ='9999-01-01'
+ORDER BY e.emp_no, t.to_date DESC;
+
+-- OTHER QUERIES FOR ANALYSIS
+SELECT DISTINCT ON (e.emp_no) e.emp_no, t.title
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE t.to_date = '9999-01-01'
+ORDER BY e.emp_no, t.to_date DESC;
+
+-- How many current employees by department?
+SELECT COUNT(de.emp_no), d.dept_name
+FROM dept_emp as de
 INNER JOIN departments as d
-ON (de.dept_no = d.dept_no);
+ON de.dept_no = d.dept_no
+WHERE de.to_date = '9999-01-01'
+GROUP BY d.dept_name;
 
-SELECT * FROM dept_info;
+-- Find the ratio of retirees to potential mentors
+SELECT title, COUNT(emp_no) "Potential Mentors"
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY "Potential Mentors" DESC;
 
-SELECT ce.emp_no,
-	ce.first_name,
-	ce.last_name,
-	d.dept_name
---INTO dept_info
-FROM current_emp as ce
-INNER JOIN dept_emp as de
-ON (ce.emp_no = de.emp_no)
-INNER JOIN departments as d
-ON (de.dept_no = d.dept_no)
-WHERE d.dept_name in ('Sales', 'Development');
+SELECT * FROM retiring_titles;
+
+SELECT COUNT(emp_no) FROM titles
+WHERE to_date = '9999-01-01';
+
+SELECT SUM(count) FROM retiring_titles;
+
+SELECT * FROM departments;
+
